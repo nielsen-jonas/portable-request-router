05891607b7ef657531fec1518d99e2058394845a
Jonas Nielsen <dev@jonas92.com>
2016-07-01 14:07:23 +0200

    Update Demo

--- .protected/usr/script/page/default.php
+++ .protected/usr/script/page/default.php
@@ -1,2 +1,12 @@
-default script</br>
-<a href="<?= scr_url('page/index'); ?>">index</a>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<title>Page Not Found | Portable request router</title>
+		<link rel="stylesheet" type="text/css" href="<?= SRC_CSS . 'style.css' ?>">
+		<link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Open+Sans">
+	</head>
+	<body>
+		<h1>Default script</h1>
+		<a href="<?= scr_url('page/index'); ?>">index</a>
+	</body>
+<html>
--- .protected/usr/script/page/index.php
+++ .protected/usr/script/page/index.php
@@ -1,8 +1,18 @@
-index script
-<hr>
-Dynamic request "<?= WEBSITE_URL ?>/dynamic/{type}/{id}":
-<ul>
-<?php foreach($_REQUEST as $key => $val): ?>
-	<li>Key: <?= $key ?>, Value: <?= $val ?></li>
-<?php endforeach; ?>
-</ul>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<title>Portable request router</title>
+		<link rel="stylesheet" type="text/css" href="<?= SRC_CSS . 'style.css' ?>">
+		<link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Open+Sans">
+	</head>
+	<body>
+		<h1>Index script</h1>
+		<p>Enter the following URL, type must be alphabetic and id must be numeric:</p>
+		<em><?= WEBSITE_URL ?>/dynamic/{type}/{id}</em>
+		<ul>
+		<?php foreach($_REQUEST as $key => $val): ?>
+			<li>Key: <?= $key ?>, Value: <?= $val ?></li>
+		<?php endforeach; ?>
+		</ul>
+	</body>
+<html>
